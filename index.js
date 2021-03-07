27484497c41d3fe4ae89803b32806e017683fe28
Pranab Dey <android.dey@gmail.com>
2021-03-07 22:21:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ var number = document.querySelector("input"), i,
     output='',
     rsult = document.querySelector(".result");
 
+function forLoop(n) {
+  for(i=1;i<n;i++){
+    output += `<p class='value'>${number.value}<span>x<span>${i} = ${number.value*i}</p>`;
+  }
+}
+
 function multiplication(e){
   if(isNaN(number.value)){
   rsult.innerHTML = `<div class='head'>You know, what is number,
  right? (example: 1, 4, 6)</div>`;
   }
   else if(number.value.length==0){
-    rsult.innerHTML = `<div class='head'>Oops!! I don't see anything up 
-there, Try enter some Numbers and See the Magic!</div>`;
+    rsult.innerHTML = `<div class='head'>Oops!! I don't see anything up there, Try enter some Numbers and See the Magic!</div>`;
   }
   else{
     output='';
     let num = document.querySelector('.multiNumber');
     let numValue = num.value;
     num.oninput = function() {
-       numValue = this.value; 
+       forLoop(this.value);
     }
-    for(i=1;i<numValue;i++){
-      output += `<p class='value'>${number.value}<span>x<span>${i} = ${number.value*i}</p>`;
-    }
+    forLoop(numValue);
     rsult.innerHTML = `<div class='head'>This is ${number.value}'s Multiplication Table</div> 
     ${output}`;
   }
